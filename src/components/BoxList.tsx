c22c86c73e6a7c4f2e1390dab3065615bbdc007a
gizemmdn <gizemmaden15@gmail.com>
2022-01-17 23:02:10 +0300

    feat: show favorites

--- src/components/BoxList.tsx
+++ src/components/BoxList.tsx
@@ -1,15 +1,23 @@
+import React, { useMemo } from 'react';
 import { useEcom } from '../context/context';
 import { addProductToFavorites } from '../context/ActionCreators';
+import { useLocation } from 'react-router-dom';
 
 const BoxList = () => {
-  const { state, dispatch } = useEcom();
+  const { state: { products, favorites }, dispatch } = useEcom();
+  const location = useLocation();
+  const mode = useMemo(() => location.pathname, [location]);
+  const currentProducts = useMemo(() => {
+    return mode === '/favorites' ? products.filter(product => favorites.find(fav => fav.id === product.id) !== undefined) : products;
+  }, [products, favorites, mode]);
+
   const handleClick = (product: object) => {
     // add to favs for now
     dispatch(addProductToFavorites(product));
   }
   return (
     <div className='productList'>
-      {state.products.map(product => (
+      {currentProducts.map(product => (
         <div className='productItem' key={product.id} onClick={() => { handleClick(product); }}>
           <img src={product.src} alt="product" />
           <div className='details'>
